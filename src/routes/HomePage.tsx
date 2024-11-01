6e926a01e9d2910a8a618a5ef740debe655aae12
hungpm671 <hungpm671@gmail.com>
2024-11-01 20:44:07 +0700

    first commit

---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,80 @@
+import { Button, Flex, Grid, Spinner } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import PokemonCard from "../component/PokemonCard";
+import ReactPaginate from "react-paginate";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { useSearchParams } from "react-router-dom";
+
+export type GetListPokemonResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Pokemon[];
+};
+
+type Pokemon = {
+  name: string;
+  url: string;
+};
+
+export default function HomePage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get("page") || 1);
+  const { data, isLoading } = useQuery({
+    queryKey: ["pokemons", page],
+    queryFn: async () =>
+      fetch(
+        `https://pokeapi.co/api/v2/pokemon?offset=${(page - 1) * 20}&limit=20`
+      ).then((res) => res.json() as Promise<GetListPokemonResponse>),
+  });
+
+  const pageCount = Math.ceil((data?.count || 0) / 20);
+
+  return (
+    <Flex flexDirection={"column"}>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          data?.results.map((pokemon) => (
+            <PokemonCard key={pokemon.name} name={pokemon.name} />
+          ))
+        )}
+      </Grid>
+
+      <Flex justifyContent={"center"} marginTop={5} gap={1}>
+        <ReactPaginate
+          forcePage={page - 1}
+          onPageChange={({ selected }) => {
+            const params = Object.fromEntries(searchParams.entries());
+
+            if (selected === 0) {
+              delete params.page;
+            } else {
+              params.page = String(selected + 1);
+            }
+            setSearchParams(params);
+          }}
+          breakLabel={<Button>...</Button>}
+          nextLabel={
+            <Button>
+              <ChevronRightIcon />
+            </Button>
+          }
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          previousLabel={
+            <Button>
+              <ChevronLeftIcon />
+            </Button>
+          }
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          pageLabelBuilder={(page) => <Button>{page}</Button>}
+          activeClassName="active"
+        />
+      </Flex>
+    </Flex>
+  );
+}
---
+++ src/routes/Root.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import Header from "../component/Header";
+
+export default function Root() {
+  return (
+    <Container maxWidth={"container.xl"}>
+      <Header />
+
+      <Outlet />
+    </Container>
+  );
+}
